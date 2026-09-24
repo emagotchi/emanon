03e0f556f7457b1bd20e4ae65fe3e11a98adbe0e
emagotchi <tsusukki@gmail.com>
2026-09-24 17:38:12 -0300

    Update data.js

--- links/js/data.js
+++ links/js/data.js
@@ -11,42 +11,42 @@ const VIDEOS = [
       {
         name: "Cable UART con conexión USB",
         url: "https://a.aliexpress.com/_mtwSZ11",
-        img: "",
+        img: "https://ae-pic-a1.aliexpress-media.com/kf/S3aeaa21b119b40fbbe4f703a6b3407a59.jpg_960x960q75.jpg_.avif",
         price: "US$8.49",
         note: "el que uso yo en el vídeo :3"
       },
       {
         name: "Cables jumper/de prueba",
         url: "https://a.aliexpress.com/_mO2ebDv",
-        img: "",
+        img: "https://ae-pic-a1.aliexpress-media.com/kf/S6e447b0b734f49c6b4ef557f5d3856ddW.jpg?has_lang=1&ver=1_960x960q75.jpg_.avif",
         price: "US$6.97",
-        note: ""
+        note: "los cables con ganchito y pin macho"
       },
       {
         name: "ko-fi Marcel's Tama Spa & Repair",
         url: "https://ko-fi.com/tamaspa/shop",
-        img: "",
+        img: "https://storage.ko-fi.com/cdn/useruploads/035985f1-01eb-407a-b7a8-506d0d3c193d_2efeca41-f1fc-401b-bda9-97bd1963a724.png",
         price: "",
         note: ""
       },
       {
         name: "ko-fi catalystwyrm",
         url: "https://ko-fi.com/catalystwyrm/shop",
-        img: "",
+        img: "https://storage.ko-fi.com/cdn/useruploads/2bc61c50-30b8-4391-83c8-8517ed613cd6_47b7a664-d263-43f1-a437-3414410c85e7.png",
         price: "",
         note: ""
       },
       {
         name: "Patchi Lab",
         url: "https://ko-fi.com/s/1414b5336f",
-        img: "",
+        img: "https://storage.ko-fi.com/cdn/useruploads/display/6ec92373-546c-43cf-8a24-290e8e9ed63f_patchilab1.png",
         price: "US$0",
         note: ""
       },
       {
         name: "TamaHome",
         url: "https://ko-fi.com/s/1843fe1f03",
-        img: "",
+        img: "https://storage.ko-fi.com/cdn/useruploads/display/9fda8f25-f258-4502-8d18-8a3dcd2e547e_screenshot2026-01-20012403.png",
         price: "US$7",
         note: ""
       },
@@ -74,7 +74,7 @@ const VIDEOS = [
       {
         name: "ko-fi Gatusita",
         url: "https://ko-fi.com/gatusita/shop",
-        img: "",
+        img: "https://storage.ko-fi.com/cdn/useruploads/d22476b8-02ba-41fb-bf98-603947f27202_de7f1ee8-5355-4785-b46e-f8beff2805e2.png",
         price: "",
         note: ""
       }
